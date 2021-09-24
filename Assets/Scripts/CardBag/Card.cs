880e3e35abb0819870e06a7a99f1ce0d0975d5ab
Ask-Fang <fang@ask-digital.co.jp>
2021-09-24 15:55:36 +0900

    card rotation

--- Assets/Scripts/CardBag/Card.cs
+++ Assets/Scripts/CardBag/Card.cs
@@ -4,26 +4,44 @@ using FairyGUI;
 public class Card : GButton
 {
     GTextField _title;
-    // Controller _readController;
-    // Controller _fetchController;
-    Transition _FlyIn;
-    Transition _FlyOut;
 
     GObject _back;
-    GObject _front;
+    GComponent _front;
+    GGraph _mask_radius;
+    GGraph _particle;
+
+    Transition _FlyIn;
+    Transition _FlyOut;
+    Transition _Turn;
+    Transition _TurnBack;
+    Transition _TurnFront;
+
+    public Card()
+    {}
 
     public override void ConstructFromXML(FairyGUI.Utils.XML cxml)
     {
         base.ConstructFromXML(cxml);
 
-        _title = this.GetChild("title").asTextField;
         _back = GetChild("back");
-        _front = GetChild("front");
+        _front = GetChild("front").asCom;
+        _mask_radius = GetChild("mask_radius").asGraph;
+        _title = _front.GetChild("title").asTextField;
         _back.visible = false;
-        // _readController = this.GetController("IsRead");
-        // _fetchController = this.GetController("c1");
+
+        // Particle
+        _particle = this.GetChild("particle").asGraph;
+
+        Object prefab = Resources.Load("Star");
+        GameObject go = (GameObject)Object.Instantiate(prefab);
+        _particle.SetNativeObject(new GoWrapper(go));
+        
+        // Transition
         _FlyIn = this.GetTransition("FlyIn");
         _FlyOut = this.GetTransition("FlyOut");
+        _Turn = this.GetTransition("Turn");
+        _TurnBack = this.GetTransition("TurnBack");
+        _TurnFront = this.GetTransition("TurnFront");
     }
 
     public void setTitle(string value)
@@ -40,7 +58,6 @@ public class Card : GButton
     // {
     //     _fetchController.selectedIndex = value ? 1 : 0;
     // }
-
     public void FlyIn(float delay)
     {
         this.visible = false;
@@ -51,7 +68,32 @@ public class Card : GButton
         this.visible = false;
         _FlyOut.Play(1, delay, null);
     }
+    
+    /// <summary>
+    /// 调用FairyGUI的动效，能设置重复次数，但无法整体设置EaseType，不推荐
+    /// </summary>
+    /// <param name="times"></param>
+    /// <param name="roundPerSecond"></param>
+    /// <param name="delay"></param>
+    public void Turn(int times, float roundPerSecond, float delay)
+    {
+        _Turn.timeScale = roundPerSecond * 2; // 2秒/圈
+        // this.visible = false;
+        _Turn.Play(times, delay, null);
+    }
+    
+    public void TurnBack(float delay)
+    {
+        // this.visible = false;
+        _TurnBack.Play(1, delay, null);
+    }
 
+        public void TurnFront(float delay)
+    {
+        // this.visible = false;
+        _TurnFront.Play(1, delay, null);
+    }
+    
     public bool opened
     {
         get
@@ -72,21 +114,30 @@ public class Card : GButton
     {
         _front.displayObject.perspective = true;
         _back.displayObject.perspective = true;
+        _mask_radius.displayObject.perspective = true;
+        _particle.displayObject.perspective = true;
+    
+        // this.displayObject.perspective = true;
     }
 
-    public void Turn()
+    public void Rotate(float round, float duration)
     {
+        this.SetPerspective();
+
         if (GTween.IsTweening(this))
             return;
 
         bool toOpen = !_front.visible;
-        GTween.To(0, 180, 0.8f).SetTarget(this).SetEase(EaseType.QuadOut).OnUpdate(TurnInTween).SetUserData(toOpen);
+        GTween.To(0, round * 360, duration).SetTarget(this).SetEase(EaseType.QuadOut).OnUpdate(TurnInTween).SetUserData(toOpen);
     }
 
     void TurnInTween(GTweener tweener)
     {
         bool toOpen = (bool)tweener.userData;
         float v = tweener.value.x;
+        _mask_radius.rotationY = v;
+        _particle.rotationY = -180 + v;
+
         if (toOpen)
         {
             _back.rotationY = v;
